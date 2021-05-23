41f76228cbdb00faf3d3bb7e06e3214bfccbd650
Matthew DeRoxtro <59454640+mderoxtro@users.noreply.github.com>
2021-05-23 10:02:26 -0400

    Handlebars Templating - Logo Re-Design - Page Layout Redesign

--- src/app.js
+++ src/app.js
@@ -1,25 +1,44 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 const pubPath = path.join(__dirname, '../public')
+const partialsPath = path.join(__dirname, '../src/partials')
 
+app.set('view engine', 'hbs');
+hbs.registerPartials(partialsPath);
 app.use(express.static(pubPath))
 
+app.get('', (req, res) => {
+  res.render('index', {
+    title: "3DeRox | 3D Printed Car Parts",
+    test: "Testing Text"
+  })
+})
+
 app.get('/about', (req, res) => {
-  res.send("About Page")
+  res.render('about', {
+    title: "3DeRox | About Us"
+  })
 })
 
 app.get('/contact', (req, res) => {
-  res.send("Contact Page")
+  res.render('contact', {
+    title: "3DeRox | Contact Us"
+  })
 })
 
 app.get('/parts', (req, res) => {
-  res.send("Parts Page")
+  res.render('parts', {
+    title: "3DeRox | Parts"
+  })
 })
 
 app.get('/projects', (req, res) => {
-  res.send("Projects Page")
+  res.render('projects', {
+    title: "3DeRox | Projects"
+  })
 })
 
 app.listen(3000, () => {
